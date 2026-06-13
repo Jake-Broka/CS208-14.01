c2d11a32e768e56a2b825eca22252c7eca4a066f
Jake Broka <brokajsb@gmail.com>
2026-06-13 23:46:18 +0000

    Completed initial functionality. Populating databases and adding more to each page next

--- routes/index.js
+++ routes/index.js
@@ -3,35 +3,83 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next){
-  try {
-    req.db.query('SELECT * FROM todos;', (err, results) => {
-      if (err) {
-        console.error('Error fetching todos:', err);
-        return res.status(500).send('Error fetching todos');
-      }
-      res.render('index', { title: 'My Simple TODO', todos: results });
-    });
+  res.render('index', { title: 'Downtown Donuts'});
+});
+
+/*GET menu page */
+router.get('/menu', function(req, res, next){
+  try{
+    req.db.query('SELECT * FROM menu', (err, results) => {
+    if(err) {
+      console.error('Error fetching menu:', err);
+      return res.status(500).send('Error fetching reviews');
+    }
+    res.render('menu', {title: 'Menu', menu: results});
+    })
   } catch (error) {
-    console.error('Error fetching items:', error);
-    res.status(500).send('Error fetching items');
+    console.error('Error fetching menu:', error);
+    res.status(500).send('Error fetching menu');
   }
 });
 
-router.post('/create', function (req, res, next) {
-    const { task } = req.body;
-    try {
-      req.db.query('INSERT INTO todos (task) VALUES (?);', [task], (err, results) => {
-        if (err) {
-          console.error('Error adding todo:', err);
-          return res.status(500).send('Error adding todo');
-        }
-        console.log('Todo added successfully:', results);
-        // Redirect to the home page after adding
-        res.redirect('/');
-      });
-    } catch (error) {
-      console.error('Error adding todo:', error);
-      res.status(500).send('Error adding todo');
+/* GET item page*/
+router.get('/menu/:item', function(req, res, next){
+  const menuItem = req.params.item;
+  try{
+    req.db.query('SELECT * FROM menu WHERE item = ?', [menuItem], (err, results) => {
+    if(err) {
+      console.error('Error fetching item:', err);
+      return res.status(500).send('Error fetching item');
+    }
+    res.render('item', {title: results[0].item, details: results[0]});
+    })
+  } catch (error) {
+    console.error('Error fetching menu:', error);
+    res.status(500).send('Error fetching menu');
+  }
+});
+
+/*GET about page */
+router.get('/about', function(req, res, next){
+  res.render('index', {title: 'About Us'});
+});
+
+/*GET reviews page */
+router.get('/reviews', function(req, res, next){
+  try{
+    req.db.query('SELECT * FROM reviews', (err, results) => {
+    if(err) {
+      console.error('Error fetching reviews:', err);
+      return res.status(500).send('Error fetching reviews');
+    }
+    res.render('reviews', {title: 'Reviews', reviews: results});
+    })
+  } catch (error) {
+    console.error('Error fetching reviews:', error);
+    res.status(500).send('Error fetching reviews');
+  }
+});
+
+/* POST reviews */
+router.post('/reviews/send', function (req, res, next) {
+    const { review } = req.body;
+    if(review.trim()){
+      try {
+        req.db.query('INSERT INTO reviews (review) VALUES (?);', [review], (err, results) => {
+          if (err) {
+            console.error('Error adding review:', err);
+            return res.status(500).send('Error adding review');
+          }
+          console.log('Review added successfully:', results);
+          // Redirect to the review page after adding
+          res.redirect('/reviews');
+        });
+      } catch (error) {
+        console.error('Error adding review:', error);
+        res.status(500).send('Error adding review');
+      }
+    } else {
+      console.log("Blank edit input!");
     }
 });
 
--- setup_scripts/create_demo_table.sql
+++ setup_scripts/create_demo_table.sql
@@ -2,8 +2,24 @@ CREATE DATABASE IF NOT EXISTS cs208demo;
 
 USE cs208demo;
 
-CREATE TABLE IF NOT EXISTS todos (
+CREATE TABLE IF NOT EXISTS menu (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    task VARCHAR(255) NOT NULL,
-    completed BOOLEAN DEFAULT FALSE
+    item VARCHAR(255) NOT NULL,
+    price_sm DECIMAL(5,2),
+    price_md DECIMAL(5,2),
+    price_lg DECIMAL(5,2),
+    `description` VARCHAR(1000),
+    `type` VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS reviews (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    review VARCHAR(255) NOT NULL,
+    `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO menu (item, price_sm, price_md, price_lg, `description`, `type`)
+VALUES('Coffee', 2.50, 0, 0, 'A yummy drink', 'coffee');
+
+INSERT INTO reviews(review)
+VALUES('Pretty good coffee');
